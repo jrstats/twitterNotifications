6e2b979622650289128eb65a8cb34f50cb6d316c
James Robinson <jrstats@outlook.com>
2022-01-09 18:33:38 +0000

    develop initial idea

--- playground-twitter.py
+++ playground-twitter.py
@@ -1,8 +1,31 @@
 # %%
 import json
 import twitter as tw
+import pandas as pd
+from csv import writer
 
+def filterTweets(tweets, terms, lower=True):
+    if lower:
+        return [t for t in tweets if any([x.lower() in t.text.lower() for x in terms])]
+    else:
+        return [t for t in tweets if any([x in t.text for x in terms])]
 
+def checkNewTweets(tweets, idCsvLocation="./id.csv"):
+    try:
+        oldTweets = pd.read_csv(idCsvLocation)
+    except FileNotFoundError:
+        print("idCsv not found - all tweets new.")
+        return tweets
+
+    return [t for t in tweets if t.id not in oldTweets["id"]]
+
+def writeNewTweets(tweets, idCsvLocation="./id.csv"):
+    with open(idCsvLocation, 'a', newline="") as f:
+        writer_object = writer(f)
+        for t in tweets:
+            writer_object.writerow([t.id])
+
+    return None
 # %%
 with open("./keys.json", "r") as f:
     keys = json.load(f)
@@ -17,18 +40,24 @@ api = tw.Api(
 # %%
 
 tweets = api.GetUserTimeline(
-    screen_name="richardosman", 
+    screen_name="BrentfordFC", 
     exclude_replies=True, 
     include_rts=False,
     count=100
 )
 # %%
-t = tweets[0]
-print(t.text)
-print(t.user.name)
-print(t.created_at)
-print([x.text for x in t.hashtags])
+ticketTerms = ["🎟", "ticket", "tickets", "all my bees members"]
+ticketTweets = filterTweets(tweets, ticketTerms)
+# %%
+for t in ticketTweets:
+    print("----------")
+    print(t.text)
+    print(t.user.name)
+    print(t.created_at)
+    print(t.id)
+    print([x.text for x in t.hashtags])
 # %%
 
-# from trafilatura import feeds
-# mylist = feeds.find_feed_urls('https://www.theguardian.com/')
+ticketTweetsNew = checkNewTweets(ticketTweets)
+writeNewTweets(ticketTweetsNew)
+# %%
